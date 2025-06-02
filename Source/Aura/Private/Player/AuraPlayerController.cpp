dcffa25f26be31392f46a31b3dba5e10ed552966
Camperfire51 <camperfire51@gmail.com>
2025-06-02 22:37:07 +0300

    14. Highlight Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,11 +4,68 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include <Interaction/EnemyInterface.h>
 
 
 AAuraPlayerController::AAuraPlayerController() 
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/*
+	* Line trace from cursor. There are several scenerios:
+	* A. LastActor is nullptr, ThisActor is nullptr: Do nothing. Easy.
+	* B. LastActor is nullptr, ThisActor is valid: We are hovering over this actor for the first time. Highlight ThisActor.
+	* C. LastActor is valid, ThisActor is nullptr: We are no longer hovering over LastActor. Unhighlight LastActor.
+	* D. LastActor is valid, ThisActor is valid but LastActor != ThisActor: We are hovering over a different actor. Unhighlight LastActor, highlight ThisActor.
+	* E. LastActor is valid, ThisActor is valid and LastActor == ThisActor: We are still hovering over the same actor. Do nothing.
+	*/
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			//Case B: Highlight ThisActor
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			//Case A: Do nothing, unnecessary but for clarity
+		}
+	}
+	else // LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			//Case C: Unhighlight LastActor
+			LastActor->UnhighlightActor();
+		}
+		else // Both Actors are valid.
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnhighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -54,3 +111,5 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
+
+
