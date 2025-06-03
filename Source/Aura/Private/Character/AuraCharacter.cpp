47187d250df0f7b00af7db195b37c16d15a70c95
Camperfire51 <camperfire51@gmail.com>
2025-06-03 15:48:43 +0300

    23. Init Ability Actor Info

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "Player/AuraPlayerState.h"
+#include "AbilitySystemComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 AAuraCharacter::AAuraCharacter()
@@ -14,4 +16,26 @@ AAuraCharacter::AAuraCharacter()
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
+}
+
+// Init ability actor info for the Server
+void AAuraCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	InitAbilityActorInfo();
+}
+
+// Init ability actor info for the Client
+void AAuraCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	InitAbilityActorInfo();
+}
+
+void AAuraCharacter::InitAbilityActorInfo()
+{
+	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
+	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
 }
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -28,6 +28,6 @@ protected:
 
 	UPROPERTY();
 	TObjectPtr<UAttributeSet> AttributeSet;
-	
+
 	
 };
