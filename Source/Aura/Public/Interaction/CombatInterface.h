7a01fbe5513a02659a556fe673f648b2d9d7c167
Camperfire51 <camperfire51@gmail.com>
2025-06-25 18:36:44 +0300

    185. Tagged Montage

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -4,9 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
+#include "GameplayTagContainer.h"
 #include "CombatInterface.generated.h"
 
-class UAnimMontage;
+USTRUCT(BlueprintType)
+struct FTaggedMontage {
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UAnimMontage* Montage = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag MontageTag;
+};
 
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI, BlueprintType)
@@ -14,6 +24,8 @@ class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
 };
+
+class UAnimMontage;
 
 /**
  * 
@@ -41,4 +53,7 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	AActor* GetAvatar();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	TArray<FTaggedMontage> GetAttackMontages();
 };
